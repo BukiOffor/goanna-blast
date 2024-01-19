149ae9354f5fd195cfca27bcc140207201f3cfd0
Ebuka <ebuka2264@yahoo.com>
2024-01-19 10:54:05 +0100

    feat: update crypto pay transcations

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const ethers = require("ethers");
 const { abi, code } = require("./constants");
 
+// Json Rpc provider to the blast network
 const provider = new ethers.JsonRpcProvider('https://rpc.ankr.com/blast_testnet_sepolia');
 
+//wallet instance of the contract admin
 const admin = new ethers.Wallet("b72cb42b3319abb30fc17f7e20ea58165a84de90c9afd90fcb80382062e01382", provider);
 
 
@@ -10,7 +12,14 @@ async function main(){
     await create_and_list_product()
 }
 
+
+//This function creates a product on the blockchain where
+    // product_id: the product of the product being listed on chain
+    // amount: price of the product
+    // merchant_id: user_id of the project creator
+    // creator: wallet address of the project creator
 async function create_and_list_product(product_id, amount, merchant_id,creator){
+    // address of the smart contract on the blast blockchain
     const marketplaceAddr = "0x686690ef4a57F11A4980e0053E2D1EdD69782F35";
     const contract = new ethers.Contract(marketplaceAddr, abi, admin);
     let list_product = await contract.createProduct(product_id,amount,merchant_id,creator);
@@ -21,10 +30,12 @@ async function create_and_list_product(product_id, amount, merchant_id,creator){
 }
 
 
+
+
 async function order_product(product_id,amount,buyer_id,wallet){
     const marketplaceAddr = "0x686690ef4a57F11A4980e0053E2D1EdD69782F35";
     const contract = new ethers.Contract(marketplaceAddr, abi, wallet);
-    let order = await contract.createProduct(product_id,amount,buyer_id);
+    let order = await contract.orderProduct(product_id,amount,buyer_id, {value:ethers.parseEther(amount)});
     let tx = await order.wait();
     console.log(order);
     console.log(tx);
